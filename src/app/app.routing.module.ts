084d1cd27acf2f1a7b9e8adee10582f400ac3fa3
JunSungKang <kjuns1@naver.com>
2018-06-21 17:43:58 +0900

    url error patch
    URL Direct Connection 404 Error.
    Patch Complete.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { LoginComponent } from './account/login/login.component';
  
 const routes: Routes = [
-  { path: '', redirectTo: '/account/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/notice', pathMatch: 'full' },
   { path: 'account', loadChildren: './account/account.module#AccountModule' },
   { path: 'notice', loadChildren: './notice/notice.module#NoticeModule' }
 ];
  
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
