c8cee73a9852cde631494054e323629bbc8a69d1
JunSungKang <kjuns1@naver.com>
2018-12-23 22:54:57 +0900

    board add update

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+
+  constructor() { }
+  date: Date = new Date();
+  htmlContent: string;
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '20rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    uploadUrl: 'v1/images', // if needed
+    customClasses: [ // optional
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
+
+  ngOnInit() {
+  }
+
+  writer(){
+    console.log(this.htmlContent);
+  }
+
+}
---
+++ src/app/board/board.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BoardRoutingModule } from './board-routing.module';
+import { BoardComponent } from './board.component';
+
+import { FormsModule } from '@angular/forms';
+import { DateFormatModule } from '../common/pipe';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+
+@NgModule({
+  declarations: [BoardComponent],
+  imports: [
+    CommonModule,
+    BoardRoutingModule,
+    FormsModule,
+    DateFormatModule,
+    AngularEditorModule
+  ]
+})
+export class BoardModule { }
