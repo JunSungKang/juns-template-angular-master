12df8d7b32c75399538dc2fbb9b1d9660d51338e
jskang <jskang@wisenut.co.kr>
2019-12-02 19:55:02 +0900

    Update: Dashboard view make up

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,4 +15,46 @@ export class DashboardComponent implements OnInit {
     console.log();
   }
 
+  public chartLabels:string[] = ["SouthKorea", "USA", "Japan", "China", "Canada"];
+  public chartData:number[] = [21, 39, 10, 14, 16];
+  public chartType:string = 'line';
+  public chartLineTypeOptions:any = {
+    maintainAspectRatio: false,
+    'backgroundColor': [ "#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB" ],
+    title: {
+      display: true,
+      text: '[ ' +this.chartType+ ' Chart ]'
+    }
+  };
+
+  public chartPieTypeOptions:any = {
+    maintainAspectRatio: false,
+    'backgroundColor': [ "#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB" ],
+    title: {
+      display: true,
+      text: '[ ' +this.chartType+ ' Chart ]'
+    }
+  };
+
+  data = {
+    "th": ["ProductID", "ProductName", "RecipeName", "RecipeType", "Register", "CreateDate", "DeleteDate"],
+    "tr": [
+      ["PRE-01", "Monitor", "AZ-01", "IN", "JSKANG", "2018.12.16.", ""],
+      ["PRE-02", "Monitor", "AZ-01", "IN", "JSKANG", "2018.12.16.", ""],
+      ["PRE-03", "Monitor", "AZ-02", "OUT", "JHKIM", "2018.12.16.", "2018.12.17."],
+      ["PRE-04", "Monitor", "AZ-02", "OUT", "JHKIM", "2018.12.16.", "2018.12.17."],
+      ["PRE-05", "Monitor", "AZ-01", "IN", "JSKANG", "2018.12.16.", ""]
+    ]
+  }
+
+  // events on slice click
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  // event on pie chart slice hover
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,11 +4,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { GridModule } from '../common/component';
+
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    ChartsModule,
+    GridModule
   ]
 })
 export class DashboardModule { }
