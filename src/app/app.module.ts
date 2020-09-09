2eea70a85739011c7ec7472a68108b4f6820a389
jskang <kjuns1@naver.com>
2020-09-10 02:23:10 +0900

    project directory constructure change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { LayoutModule } from './common/layout/layout.module';
+import { LayoutModule } from './layout/layout.module';
 
 import { AppRoutingModule } from './app.routing.module';
 
